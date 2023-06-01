4147267ef8beecd3dfda253f645c444226b001f6
Gbc6292 <6292@holbertonstudents.com>
2023-06-01 14:46:48 -0400

    correcting negative value in task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 last = int(str(number)[-1]) * 1
 if last > 5:
+    if number < 0:
+        last = last * -1
+    else:
+        pass
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
     print(f"Last digit of {number} is {last} and is 0")
 elif last < 6 != 0:
+    if number < 0:
+        last = last * -1
+    else:
+        pass
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
