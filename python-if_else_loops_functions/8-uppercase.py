c1cc572cc77940be01dc0303df2f1c93499a4402
Gbc6292 <6292@holbertonstudents.com>
2023-06-02 21:43:28 -0400

    Correcting new line

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,8 @@ def uppercase(str):
     for i in str:
         if ord('a') <= ord(i) <= ord('z'):
             upper = chr(ord(i) - 32)
-            print("{}".format(upper), end='')
 
         else:
             upper = i
-            print("{}".format(upper), end='')
+        print("{}".format(upper), end='')
     print()
