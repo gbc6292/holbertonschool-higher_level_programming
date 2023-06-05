c6d4d4283d7374fa877733fbef8b722e75ed20a9
Gbc6292 <6292@holbertonstudents.com>
2023-06-05 17:53:11 -0400

    correcting import error

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
-print('1 + 2 = {}'.format(add(1, 2)))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    x = add(a, b)
+
+print('{} + {} = {}'.format(a, b, x))
