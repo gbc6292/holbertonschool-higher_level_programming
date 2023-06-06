06fb850f4500cea79664856db6e7ab161338c1a6
Gbc6292 <6292@holbertonstudents.com>
2023-06-06 13:39:09 -0400

    Correcting output index 5

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,8 +4,8 @@ def element_at(my_list, idx):
 
     if idx < 0:
         return 'None'
-    elif idx > len(my_list):
-        return "None"
+    elif idx >= len(my_list):
+        return 'None'
     else:
         i = my_list[idx]
         return i
