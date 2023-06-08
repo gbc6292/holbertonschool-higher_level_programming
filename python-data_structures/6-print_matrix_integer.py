5d02073d8f03f58481237d3e38a30e7612503094
Gbc6292 <6292@holbertonstudents.com>
2023-06-08 10:20:19 -0400

    correcting output

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,8 @@ def print_matrix_integer(matrix=[[]]):
         if not row:
             print("")
         else:
-            print(''.join(str(element) for element in row))
+            for idx, element in enumerate(row):
+                if idx < len(row) - 1:
+                    print('{:d}'.format(element), end='')
+                else:
+                    print('{:d}'.format(element))
