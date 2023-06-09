24db1d2111aad99b45aab83bbd6b4ef230a90869
Gbc6292 <6292@holbertonstudents.com>
2023-06-08 23:03:09 -0400

    adding task 5

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    NewList = []
+    for a in range(list_length):
+        try:
+            division = my_list_1[a] / my_list_2[a]
+        except ZeroDivisionError:
+            print("division by 0")
+            division = 0
+        except TypeError:
+            print("wrong type")
+            division = 0
+        except IndexError:
+            print("out of range")
+            division = 0
+        finally:
+            NewList.append(division)
+    return NewList
