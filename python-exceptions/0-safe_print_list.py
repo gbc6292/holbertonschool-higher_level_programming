04c8cba47f97d19b7632e314635cf50230393424
Gbc6292 <6292@holbertonstudents.com>
2023-06-08 23:06:11 -0400

    Correcting output

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,6 @@ def safe_print_list(my_list=[], x=0):
             if count < x:
                 print(i, end="")
                 count += 1
-                print()
         return count
     except IndexError:
         print("x is bigger than my_list")
