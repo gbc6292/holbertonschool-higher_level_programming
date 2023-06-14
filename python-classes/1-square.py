733864dbcdd01e36f6880dba8bf7cdad2a855247
Gbc6292 <6292@holbertonstudents.com>
2023-06-14 14:43:18 -0400

    Set size attribute private in file 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" File that contain the definition of class Square """
+
+
+class Square:
+    """ square is defined """
+
+    def __init__(self, size):
+        """Adding the atribute of size to the Square class"""
+        self.__size = size
