214c5a8983a31a6b425cdd13d972ce5856c99566
Gbc6292 <6292@holbertonstudents.com>
2023-06-15 09:36:40 -0400

    correcting output task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,11 +5,11 @@
 class Square:
     """ square is defined """
 
-    def __init__(self, size: int(0)):
+    def __init__(self, size=0):
         """size must be an integer and greather than 0"""
-        if isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif isinstance(size) >= 0:
-            raise ValueError('size must be >= 0')
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
