b561ea71ece4d6fea987fa36d924168d1ef54c7a
Gbc6292 <6292@holbertonstudents.com>
2023-06-15 11:56:59 -0400

    Adding task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" File that contain the definition of class Square """
+
+
+class Square:
+    """Initializing the class Square"""
+
+    def __init__(self, size=0):
+        """ square is defined """
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the private instance 'size'"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setting the value from the private instance size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Defining area atribute"""
+        a = self.size * self.size
+        return a
+
+    def my_print(self):
+        """Printing the square using '#' character"""
+        for x in range(self.size):
+            for y in range(self.size):
+                print('#', end='')
+            print()
