5a63202d9f252d4af73a3f85f96b3ba47618406a
Gbc6292 <6292@holbertonstudents.com>
2023-06-21 21:32:27 -0400

    Adding task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Creating a empty class"""
+
+
+class BaseGeometry():
+    """Creating a public instance method
+    """
+
+    def area(self):
+        """adding an exception"""
+        raise Exception('area() is not implemented')
