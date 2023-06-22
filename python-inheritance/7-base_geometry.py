60b1f9d7b93f2a6d0db2fc93cddc02aaf8a41539
Gbc6292 <6292@holbertonstudents.com>
2023-06-22 16:45:21 -0400

    Correcting output task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,15 +3,16 @@
 
 
 class BaseGeometry():
-    """Creating a public instance method
-    """
+    """Creating a public instance method"""
 
     def area(self):
-        """adding an exception"""
+        """Adding an exception"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f'{name}must be an integer')
-        elif value <= 0:
-            raise TypeError(f"{name} must be greater than 0")
+        """Validate if the value is an integer and greater than 0"""
+        if isinstance(name, str):
+            if type(value) is not int:
+                raise TypeError(f'{name} must be an integer')
+            elif value <= 0:
+                raise TypeError(f'{name} must be greater than 0')
