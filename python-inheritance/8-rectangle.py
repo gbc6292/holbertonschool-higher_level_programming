91019705ea6481b1d35ab6c40e7689d9c16ca13a
Gbc6292 <6292@holbertonstudents.com>
2023-06-22 20:32:19 -0400

    Adding task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 """Creating a empty class"""
 
 
@@ -18,3 +16,15 @@ class BaseGeometry():
                 raise TypeError(f'{name} must be an integer')
             elif value <= 0:
                 raise TypeError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    """A child class from Base Geometry"""
+
+    def __init__(self, width, height):
+        """Creating instance for width and height
+        and validate that the argument are integers"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
