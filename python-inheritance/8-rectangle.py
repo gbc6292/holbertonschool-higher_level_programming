ae4472fbdb524e1ff2c8494bc76c908d98dd880b
Gbc6292 <6292@holbertonstudents.com>
2023-06-22 22:46:08 -0400

    Adding task 10

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
 """Creating a empty class"""
 
-
-class BaseGeometry():
-    """Creating a public instance method"""
-
-    def area(self):
-        """Adding an exception"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Validate if the value is an integer and greater than 0"""
-        if isinstance(name, str):
-            if type(value) is not int:
-                raise TypeError(f'{name} must be an integer')
-            elif value <= 0:
-                raise TypeError(f'{name} must be greater than 0')
+BaseGeometry = __import__('9-rectangle').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
