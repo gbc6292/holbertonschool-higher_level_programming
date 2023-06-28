453cd90b25dda963df4c3b04177cda4001cc5d61
Gbc6292 <6292@holbertonstudents.com>
2023-06-27 21:48:16 -0400

    correcting output Rectangle Model

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,8 +8,53 @@ class Rectangle(Base):
     """Rectangle Class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Asigning arguments to the right attribute"""
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+
+    def get_width(self):
+        """Getter for width"""
+        return self.__width
+
+    def set_width(self, value):
+        """Setter for width"""
+        if value <= 0:
+            raise ValueError("Width must be greater than zero.")
+        self.__width = value
+
+    def get_height(self):
+        """Getter for height"""
+
+        return self.__height
+
+    def set_height(self, value):
+        """Setter for width"""
+        if value <= 0:
+            raise ValueError("Height must be greater than zero")
+        self.__height = value
+
+    def get_x(self):
+        """Getter for x"""
+
+        return self.__x
+
+    def set_x(self, value):
+        """setter for x"""
+        if value < 0:
+            raise ValueError("x can't be negative")
+        self.__x = value
+
+    def get_y(self):
+        """Getter for y"""
+
+        return self.__y
+
+    def set_y(self, value):
+        """setter for y"""
+        if value < 0:
+            raise ValueError("y can't be negative")
+
+        self.__y = value
--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -5,17 +5,60 @@ import unittest
 from test_base import Base
 
 
-class Rectangle(unittest.TestCase):
+class Rectangle(Base):
     """Rectangle Class"""
 
     def test__init__(self, width, height, x=0, y=0, id=None):
-        """Attributes of the class rectangle class"""
+        """Asigning arguments to the right attribute"""
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        self.__id = id
+
+    def testget_width(self):
+        """Getter for width"""
+        return self.__width
+
+    def testset_width(self, value):
+        """Setter for width"""
+        if value <= 0:
+            raise ValueError("Width must be greater than zero.")
+        self.__width = value
+
+    def testget_height(self):
+        """Getter for height"""
+
+        return self.__height
+
+    def testset_height(self, value):
+        """Setter for width"""
+        if value <= 0:
+            raise ValueError("Height must be greater than zero")
+        self.__height = value
+
+    def testget_x(self):
+        """Getter for x"""
+
+        return self.__x
+
+    def testset_x(self, value):
+        """setter for x"""
+        if value < 0:
+            raise ValueError("x can't be negative")
+        self.__x = value
+
+    def testget_y(self):
+        """Getter for y"""
+
+        return self.__y
+
+    def testset_y(self, value):
+        """setter for y"""
+        if value < 0:
+            raise ValueError("y can't be negative")
+
+        self.__y = value
 
 
 if __name__ == '__main__':
