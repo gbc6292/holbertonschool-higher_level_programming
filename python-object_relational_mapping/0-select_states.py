b07635f1da92e5d264ee65bd02a82f2352179d76
Gbc6292 <6292@holbertonstudents.com>
2023-07-31 14:53:45 -0400

    correcting outputs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,6 @@
 
 import sys
 import MySQLdb
-
 """Script that lists all states from the database hbtn_0e_0_usa"""
 
 
@@ -10,25 +9,50 @@ import MySQLdb
 
 
 def states_list(username, password, database_name):
-    """ Connection to the MySQL server """
-    database = MySQLdb.connect(
-        host='localhost',
-        username=username,
-        password=password,
-        db=database_name,
-        port=3306
-    )
-
-    cursor = database.cursor()
-
     try:
-        rows = cursor.execute('SELECT * FROM states ORDER BY id ASC')
+        """ Setting connection to the MySQL server """
+        conn = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            db=database_name,
+            host='localhost',
+            port=3306
+        )
+        """Creating cursor to execute queries"""
+        cur = conn.cursor()
+
+        """Execute query"""
+        query = 'SELECT * FROM states ORDER BY id ASC'
+        cur.execute(query)
+
+        """Fetch all the rows returned by the query"""
+
+        rows = cur.fetchall()
+
+        """Process and print the data"""
+
         for row in rows:
             print(row)
 
-    except Exception as e:
+    except MySQLdb.Error as e:
         print('Error:', e)
 
     finally:
-        cursor.close()
-        database.close()
+
+        if 'cur' in locals():
+            cur.close()
+        if 'conn' in locals():
+            conn.close()
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: python script_name.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    states_list(username, password, database_name)
