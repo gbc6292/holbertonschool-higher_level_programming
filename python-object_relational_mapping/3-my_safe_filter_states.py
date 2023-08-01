9a2665fae798ecfe7d3c90bf4d2ae9cd4cd8966c
Gbc6292 <6292@holbertonstudents.com>
2023-08-01 13:22:57 -0400

    correcting querie and variables

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,51 +4,34 @@
 all values in the states table of hbtn_0e_0_usa
 where name matches the argument."""
 
-import sys
+
+from sys import argv
 import MySQLdb
 
-"""Function Definition"""
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    state_name = argv[4]
 
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        password=passwd,
+        db=db
+    )
 
-def states_by_name(username, password, database, state_name):
-    """ Setting connection to the MySQL server """
-    try:
-        conn = MySQLdb.connect(
-            user=username,
-            passwd=password,
-            db=database,
-            host='localhost',
-            port=3306
-        )
+    cur = conn.cursor()
+    protected = (state_name,)
+    query = """SELECT * FROM states
+            WHERE name LIKE BINARY %s
+            """.format(protected)
 
-        cur = conn.cursor()
-        query = """SELECT *
-                FROM states
-                WHERE BINARY name = %s
-                ORDER BY id ASC
-            """.format(state_name)
+    cur.execute(query, protected)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
 
-        cur.execute(query, (state_name,))
-        results = cur.fetchall()
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print('Error', e)
-
-        cur.close()
-        conn.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("""Usage: python_script.py <mysql_username>
-              <mysql_password> <database_name> <state_name>""")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = [4]
-
-    states_by_name(username, password, database, state_name)
+    cur.close()
+    conn.close()
